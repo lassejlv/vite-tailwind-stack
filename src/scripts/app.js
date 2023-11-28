c51496674365f77e768b97da145db8e7611309aa
lassev05 <vestergaardlasse2@gmail.com>
2023-11-28 21:41:00 +0100

    Third commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,9 +8,27 @@ buttons.forEach((button) => {
     href && e.preventDefault();
 
     if (href) {
+      button.removeAttribute("href");
       window.location.href = href;
     } else {
       console.error("No href attribute found on button");
     }
   });
 });
+
+// renderPage api
+const renderPage = (page) => {
+  let currentPage = page;
+  const pages = document.querySelectorAll(".page");
+
+  pages.forEach((page) => {
+    if (page.id === currentPage) {
+      page.classList.remove("hidden")
+    } else {
+      page.classList.add("hidden")
+      console.log(`${page.id} is hidden`);
+    }
+  });
+};
+
+renderPage("app");
